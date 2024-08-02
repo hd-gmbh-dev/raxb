127514ff84e69834842c96708ccde1338da8c6e7
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2024-08-02 16:49:36 +0200

    cleanup raxb-validate-wasm package

--- scripts/build-dependencies.sh
+++ scripts/build-dependencies.sh
@@ -1,27 +1,3 @@
 git clean -X -f -d
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cargo build --release
-source ${EMSDK_DIR}/emsdk_env.sh
-npm install -g typescript
-
-pushd ./crates/raxb-libxml2-sys/third_party
-mkdir -p ./libxml2-build
-mkdir -p ./libxml2/m4
-popd
-
-pushd ./crates/raxb-libxml2-sys/third_party/libxml2
-git clean -X -f
-autoreconf -if -Wall
-popd
-
-pushd ./crates/raxb-libxml2-sys/third_party/libxml2-build
-emconfigure ../libxml2/configure --disable-shared \
-    --with-minimum --with-http=no --with-ftp=no --with-catalog=no \
-    --with-python=no --with-threads=no \
-    --with-output --with-c14n --with-zlib=no \
-    --with-schemas --with-schematron
-emmake make
-popd
-pushd ./packages/raxb-validate-wasm
-./build.sh
-popd
